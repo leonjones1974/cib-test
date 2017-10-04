c68ec5dfea5fc1c321c0db34c7638ad0a0a6bd5c
Leon Jones <leonj_24@yahoo.com>
2017-10-04 11:05:42 +0100

    Fix cucumber structure and generate report

---
+++ src/test/java/uk/camsw/cib/bdd/RunBddTest.java
@@ -0,0 +1,12 @@
+package uk.camsw.cib.bdd;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(strict = false, features = "src/test/resources/features", format = { "pretty",
+        "html:target/site/cucumber-pretty",
+        "json:target/cucumber.json" }, tags = { "~@ignore" })
+
+public class RunBddTest {
+}
--- src/test/java/uk/camsw/cib/bdd/RunCukesTest.java
+++
@@ -1,9 +0,0 @@
-package uk.camsw.cib.bdd;
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(plugin = "json:target/cucumber-report.json")
-public class RunCukesTest {
-}
