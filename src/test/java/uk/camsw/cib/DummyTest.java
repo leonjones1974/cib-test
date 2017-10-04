1f4af5071ae083c36f77215e09038723ae527bf8
Leon Jones <leonj_24@yahoo.com>
2017-10-04 10:19:34 +0100

    Create failing dummy test

---
+++ src/test/java/uk/camsw/cib/DummyTest.java
@@ -0,0 +1,13 @@
+package uk.camsw.cib;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DummyTest {
+
+    @Test
+    public void itShouldFail() {
+        assertThat(false).isTrue();
+    }
+}
